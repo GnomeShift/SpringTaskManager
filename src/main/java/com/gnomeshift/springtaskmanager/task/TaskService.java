b0f13bd2171e409ec0fffdaf6cebf565c6211231
GnomeShift <151649479+GnomeShift@users.noreply.github.com>
2025-05-03 13:26:21 +0300

    fix: entity duplicate in API response

--- src/main/java/com/gnomeshift/springtaskmanager/task/TaskService.java
+++ src/main/java/com/gnomeshift/springtaskmanager/task/TaskService.java
@@ -35,7 +35,7 @@ public class TaskService {
             ownerDTO.setName(task.getOwner().getName());
             ownerDTO.setEmail(task.getOwner().getEmail());
             ownerDTO.setRoles(task.getOwner().getRoles());
-            dto.setOwner(ownerDTO);
+            dto.setOwnerId(ownerDTO.getId());
         }
 
         if (task.getAssignee() != null) {
@@ -45,7 +45,7 @@ public class TaskService {
             assigneeDTO.setName(task.getAssignee().getName());
             assigneeDTO.setEmail(task.getAssignee().getEmail());
             assigneeDTO.setRoles(task.getAssignee().getRoles());
-            dto.setAssignee(assigneeDTO);
+            dto.setAssigneeId(assigneeDTO.getId());
         }
 
         return dto;
