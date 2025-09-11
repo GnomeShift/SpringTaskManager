bc025213002c8341515a4f1c53214f9c35663405
GnomeShift <151649479+GnomeShift@users.noreply.github.com>
2025-09-11 19:31:51 +0300

    fix: missing DTO in API response

--- src/main/java/com/gnomeshift/springtaskmanager/user/UserService.java
+++ src/main/java/com/gnomeshift/springtaskmanager/user/UserService.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -34,16 +33,13 @@ public class UserService {
                 .orElseThrow(() -> new EntityNotFoundException("User not found"));
     }
 
-    public User updateUser(UUID id, User user) {
-        Optional<User> userOptional = userRepository.findById(id);
+    public UserDTO updateUser(UUID id, UserDTO userDTO) {
+        User user = userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("User not found"));
 
-        if (userOptional.isPresent()) {
-            user.setId(id);
-            return userRepository.save(user);
-        }
-        else {
-            return null;
-        }
+        user.setName(userDTO.getName());
+        user.setEmail(userDTO.getEmail());
+        user.setRoles(userDTO.getRoles());
+        return convertToDto(userRepository.save(user));
     }
 
     public void deleteUser(UUID id) {
